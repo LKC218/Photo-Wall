444061895f103cc591c3337d70c261d502c92bda
LKC218 <342679795@qq.com>
2026-07-08 01:50:42 +0800

    refactor: 移除螺旋页面及 3D 场景
    - 删除 spiral 页面组件和 3D 螺旋模型资源
    - 清理 three/scenes 中的 Spiral 场景引用

--- src/three/scenes/index.js
+++ src/three/scenes/index.js
@@ -1,4 +1,3 @@
 export { default as Billboard } from './Billboard';
 export { default as Banner } from './Banner';
 export { default as Paper } from './Paper';
-export { default as Spiral } from './Spiral';
